bfb98dd84474a539f0a7cd5e6533f5b027408a78
Zineddine Ait Rabah <zineddine@whitebay.limited>
2025-09-15 17:43:03 +0100

    sidebar control

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -4,12 +4,12 @@ import { useState } from "react";
 import ScrollToTop from "../components/scroll-to-top";
 export default function AppLayout() {
   const [navbarPlayerMode, setNavbarPlayerMode] = useState(false);
-
+  const [openSidebar, setOpenSidebar] = useState(false)
   return (
     <div className="h-full min-h-screen dark:bg-zinc-900">
       <ScrollToTop />
-      <Navbar {...{ navbarPlayerMode }} />
-      <Outlet context={{ setNavbarPlayerMode }} />
+      <Navbar {...{ navbarPlayerMode, setOpenSidebar }} />
+      <Outlet context={{ setNavbarPlayerMode, openSidebar }} />
     </div>
   );
 }
