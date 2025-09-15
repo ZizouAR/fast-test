b2ac83defac2ea5e1244792d1b0935bdcb6d8687
Zineddine Ait Rabah <zineddine@whitebay.limited>
2025-09-15 16:00:32 +0100

    no basis on mobile

--- src/layout/sidebar/layout.tsx
+++ src/layout/sidebar/layout.tsx
@@ -5,7 +5,7 @@ export default function SidebarLayout() {
   return (
     <div className="flex">
       <Sidebar />
-      <div className="basis-4/5 overflow-hidden mt-20">
+      <div className="overflow-hidden mt-20 md:basis-4/5  lg:basis-4/5">
         <Outlet />
       </div>
     </div>
