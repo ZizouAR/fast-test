09c7ec37ded940f4cd31aa2cdae16d83ce6d2667
Zineddine Ait Rabah <zineddine@whitebay.limited>
2025-09-15 18:32:38 +0100

    player sizes

--- src/partials/videos/show/player/index.tsx
+++ src/partials/videos/show/player/index.tsx
@@ -2,14 +2,13 @@ export default function ViedeoShowPlayer() {
   return (
     <div className="rounded-2xl">
       <iframe
-        width="100%"
-        height="439px"
+
         src="https://www.youtube.com/embed/fiqxEb_URX0?si=B75p41pKJK0WMhme"
         title="YouTube video player"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
         referrerPolicy="strict-origin-when-cross-origin"
         allowFullScreen
-        className="rounded-2xl"
+        className="rounded-2xl w-full h-62 md:h-[440px]"
       ></iframe>
     </div>
   );
